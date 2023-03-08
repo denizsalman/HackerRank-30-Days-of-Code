536e3b1cce879be5b6e99f3110a1833bfe93be7a
Deniz Salman <ddenizsalman@gmail.com>
2023-03-09 00:32:01 +0300

    c3

--- 6 - Let's Review/Solution.java
+++ 6 - Let's Review/Solution.java
@@ -2,6 +2,7 @@ import java.io.*;
 import java.util.*;
 
 public class Solution {
+    //test1
 
     public static void main(String[] args) {
         /* Enter your code here. Read input from STDIN. Print output to STDOUT. Your class should be named Solution. */
